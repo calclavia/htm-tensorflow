5d953e751ec4d10c236755ade2c697da41893c24
Henry Mao <admin@calclavia.com>
2016-11-04 12:51:57 -0700

    Implemented feed-forward system

--- layer.py
+++ layer.py
@@ -1,23 +1,68 @@
 import numpy as np
+from util import PriorityQueue
+from math import *
 
 class Layer:
-    def __init__(self, in_size, out_size):
+    def __init__(self, in_size, out_size, threshold=0.5):
         self.in_size = in_size
         self.out_size = out_size
+        self.threshold = threshold
+        # 2% of the output should be active
+        self.max_active = ceil(out_size * 0.02)
+
+        # Binary activation
+        self.bin_threshold = np.vectorize(lambda x: 1 if x > self.threshold else 0)
 
         # Learned parameters
         dim = (out_size, in_size)
-        self.synapse = np.matrix(np.zeros(dim), dtype='bool')
+        self.synapse = np.mat(np.zeros(dim), dtype='bool')
         # Float between 0 and 1 specifying how strong the connections are.
-        self.permanence = np.matrix(np.random.uniform(0, 1, (dim)))
-        # 1 if connected. 0 if disconnected
-        self.connections = np.matrix(np.zeros(dim), dtype='bool')
+        self.permanence = np.mat(np.random.uniform(0, 1, (dim)))
+        self.update_connections()
 
     def forward(self, input):
         """
         Perform spatial pooling algorithm.
 
         Parameters:
-            - input: Input boolean vectogir
+            - input: Input boolean vector
         """
-        pass
+        # TODO: Boost the active ones?
+        overlap_scores = input * self.connections
+
+        # Apply global inhibition (pick top n scores)
+        inhibited = self.global_inhibit(overlap_scores)
+        return self.bin_threshold(inhibited)
+
+    def global_inhibit(self, overlap_scores):
+        # Set of outputs to keep
+        keep = PriorityQueue()
+
+        for i in range(self.out_size):
+            score = overlap_scores[0, i]
+
+            if keep.empty() or score > keep.peek():
+                # We want this score to be kept
+
+                if keep.size() < self.max_active:
+                    keep.update(i, score)
+                else:
+                    # Replace the lowest score in keep with this one
+                    keep.pushpop(i, score)
+
+        # Set all indices not kept to 0
+        keep_indicies = {i for _, _, i in keep.heap}
+
+        for i in range(self.out_size):
+            if i not in keep_indicies:
+                overlap_scores[0, i] = 0
+
+        return overlap_scores
+
+    def update_connections(self):
+        """
+        Recalculate the connected synapses based on permanence and a global
+        threshold
+        """
+        # 1 if connected. 0 if disconnected
+        self.connections = self.bin_threshold(self.permanence)
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from layer import *
+import numpy as np
 
 def main():
-    print('Creating new layer')
+    print('Testing new layer')
     layer = Layer(10, 10)
+    res = layer.forward(np.mat([1,0,0,0,1,1,1,0,0,0]))
+    print(layer.connections)
+    print(res)
 
 if __name__ == '__main__':
     main()
