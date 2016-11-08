5029aea4f6411858e9427f5968144c9a082a2ccd
Henry Mao <henry@calclavia.com>
2016-11-07 22:04:37 -0800

    Added row zero-out to prevent changing weights of non-contributing rows

--- main2.py
+++ main2.py
@@ -5,8 +5,8 @@ from math import *
 
 inhibit_module = tf.load_op_library('./inhibit.so')
 
-epochs = 2
-dim = (10, 10)
+epochs = 10
+dim = [10, 10]
 sparsity = 0.2
 learning_rate = 0.1
 
@@ -15,15 +15,13 @@ output_zeros = int(dim[1] * (1 - sparsity))
 # Some input vector
 x = tf.placeholder(tf.float32)
 
-weight_dim = [10, 10]
-
 # Permanence of connections between neurons
-p = tf.Variable(tf.random_uniform(weight_dim, -1, 1), name='Permanence')
+p = tf.Variable(tf.random_uniform(dim, -1, 1), name='Permanence')
 
 # TODO: Synapse matrix for what can possibly connect
 
 # Connection matrix
-c = tf.Variable(tf.zeros(weight_dim), name='Connections')
+c = tf.Variable(tf.zeros(dim), name='Connections')
 
 """
 Operations
@@ -32,7 +30,7 @@ Operations
 p_cond = tf.greater(p, 0)
 
 # Update the connections based on permenance
-update_connections = tf.assign(c, tf.select(p_cond, tf.ones(weight_dim), tf.zeros(weight_dim)))
+update_connections = tf.assign(c, tf.select(p_cond, tf.ones(dim), tf.zeros(dim)))
 
 # Compute the activation before inhibition applied
 activation = tf.matmul(x, c)
@@ -40,6 +38,9 @@ activation = tf.matmul(x, c)
 # Output vector
 y = inhibit_module.inhibit(activation)
 
+"""
+Weight update
+"""
 # Compute the delta permanence matrix
 # Start with the connection matrix
 # Every connection that was aligned with an input = 1. 0 otherwise.
@@ -47,28 +48,52 @@ input_rowed = tf.transpose(tf.matmul(tf.transpose(x), tf.ones_like(x)))
 # Elementwise product between connection matrix and the input rows
 # Rescale values to be between -1 and 1
 alignment = (c * input_rowed - 0.5) * 2
-# TODO: Only update weights to active outputs, so zero-out non-active output rows (no change)
-delta = learning_rate * alignment
+# Only update weights to active outputs, so zero-out non-active output rows
+# We do this by constructing a diagonal matrix that scales rows to zero
+zero_out = tf.diag(y[0])
+delta = learning_rate * tf.transpose(tf.matmul(tf.transpose(alignment), zero_out))
 learn = tf.assign(p, tf.add(p, delta))
 
 init_op = tf.initialize_all_variables()
 
-# To run the matmul op we call the session 'run()' method, passing 'product'
-# which represents the output of the matmul op.  This indicates to the call
-# that we want to get the output of the matmul op back.
-#
-# All inputs needed by the op are run automatically by the session.  They
-# typically are run in parallel.
-#
-# The call 'run(product)' thus causes the execution of three ops in the
-# graph: the two constants and matmul.
-#
-# The output of the op is returned in 'result' as a numpy `ndarray` object.
+def cluster():
+    counts = [0 for _ in range(classes)]
+    clusters = [np.mat(np.zeros((1, output_size))) for _ in range(classes)]
+    for input, c in zip(images, labels):
+        clusters[c] += layer.forward(input)
+        counts[c] += 1
+
+    for c in range(len(clusters)):
+        clusters[c] /= counts[c]
+
+    return clusters
+
+def validate():
+    correct = 0
+    for input, c in zip(images, labels):
+        # Find best match in cluster
+        best_class = None
+        min_norm = inf
+        output = layer.forward(input)
+        for k, cluster in enumerate(clusters):
+            diff = np.linalg.norm(cluster - output)
+            if diff < min_norm:
+                min_norm = diff
+                best_class = k
+
+        # Validate if best class is correct
+        if c == best_class:
+            correct += 1
+    return correct / float(len(images))
+
+
 with tf.Session() as sess:
     # Run the 'init' op
     sess.run(init_op)
     sess.run(update_connections)
 
     for _ in range(epochs):
-        result, learned_p = sess.run([y, learn], feed_dict={x: [[0,0,0,0,1,0,0,0,1,0]]})
-        print(result, learned_p)
+        result1, _ = sess.run([y, learn], feed_dict={x: [[0,0,0,0,1,0,0,0,1,0]]})
+        result2, _ = sess.run([y, learn], feed_dict={x: [[1,0,0,0,1,0,0,0,0,0]]})
+        result3, _ = sess.run([y, learn], feed_dict={x: [[1,0,0,0,0,1,0,0,0,0]]})
+        print(result1, result2, result3)
