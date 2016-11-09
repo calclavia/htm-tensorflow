a413b97b7819df651aa8e98c57f03ae0837559d8
Henry Mao <admin@calclavia.com>
2016-11-08 19:25:35 -0800

    Added clustering

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from util import *
 from math import *
 from layer import *
 
-epochs = 10
+epochs = 2
 dim = [784, 10]
 sparsity = 0.2
 learning_rate = 0.1
@@ -12,18 +12,6 @@ learning_rate = 0.1
 layer = Layer(dim, sparsity, learning_rate)
 
 init_op = tf.initialize_all_variables()
-
-def cluster(output_size, classes):
-    counts = [0 for _ in range(classes)]
-    clusters = [np.mat(np.zeros((1, output_size))) for _ in range(classes)]
-    for input, c in zip(images, labels):
-        clusters[c] += layer.forward(input)
-        counts[c] += 1
-
-    for c in range(len(clusters)):
-        clusters[c] /= counts[c]
-
-    return clusters
 
 def validate():
     correct = 0
@@ -48,7 +36,7 @@ from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("data/", one_hot=False)
 
 # Process data
-input_set = [[x] for x in mnist.train.images]
+input_set = [[x] for x in mnist.train.images[:10000]]
 
 with tf.Session() as sess:
     # Run the 'init' op
@@ -60,4 +48,20 @@ with tf.Session() as sess:
             sess.run([layer.y, layer.learn], feed_dict={layer.x: x})
             progress_bar(i / float(len(input_set)))
         print()
-        #print(' == Clustering ==')
+
+        print(' == Clustering ==')
+        """
+        Take all the inputs and determine its cluster based on
+        average values.
+        """
+        counts = [0 for _ in range(dim[1])]
+        clusters = [0 for _ in range(dim[1])]
+
+        for input, label in zip(input_set, mnist.train.labels):
+            clusters[label] += sess.run(layer.y, feed_dict={layer.x: input})
+            counts[label] += 1
+
+        for c in range(len(clusters)):
+            clusters[c] /= counts[c]
+
+        print(clusters[0])
