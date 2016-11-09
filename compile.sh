47312551125bc9567acc73724c56eb182ce3e84b
Henry Mao <admin@calclavia.com>
2016-11-08 19:08:27 -0800

    Fixed compile.sh on Mac

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 TF_INC=$(python3.5 -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
-g++ --std=c++11 --shared inhibit.cc -o inhibit.so -fPIC -I $TF_INC -O2
+g++ --std=c++11 --shared inhibit.cc -o inhibit.so -fPIC -I $TF_INC -O2 #-undefined dynamic_lookup
--- main.py
+++ main.py
@@ -1,63 +1,63 @@
-from layer import *
+import tensorflow as tf
+import numpy as np
 from util import *
 from math import *
-import numpy as np
+from layer import *
 
-def main():
-    print('Loading...')
-    train_images, train_labels, images, labels = load_mnist_all(2000, 500)
+epochs = 10
+dim = [784, 10]
+sparsity = 0.2
+learning_rate = 0.1
 
-    train_size = len(train_images)
-    input_size = len(train_images[0])
-    output_size = 10
+layer = Layer(dim, sparsity, learning_rate)
 
-    print('Processing...')
-    train_images = [np.mat([min(y, 1) for y in x]) for x in train_images]
-    images = [np.mat([min(y, 1) for y in x]) for x in images]
+init_op = tf.initialize_all_variables()
 
-    print('Training...')
-    layer = Layer(input_size, output_size)
+def cluster(output_size, classes):
+    counts = [0 for _ in range(classes)]
+    clusters = [np.mat(np.zeros((1, output_size))) for _ in range(classes)]
+    for input, c in zip(images, labels):
+        clusters[c] += layer.forward(input)
+        counts[c] += 1
 
-    classes = 10
-    epoch = 10
+    for c in range(len(clusters)):
+        clusters[c] /= counts[c]
 
-    for i in range(epoch):
-        print('=== Epoch ' + str(i) + ' ===')
-        for i, input in enumerate(train_images):
-            output = layer.forward(input)
-            layer.learn(input, output)
-            progress_bar(i / float(train_size))
+    return clusters
 
+def validate():
+    correct = 0
+    for input, c in zip(images, labels):
+        # Find best match in cluster
+        best_class = None
+        min_norm = inf
+        output = layer.forward(input)
+        for k, cluster in enumerate(clusters):
+            diff = np.linalg.norm(cluster - output)
+            if diff < min_norm:
+                min_norm = diff
+                best_class = k
+
+        # Validate if best class is correct
+        if c == best_class:
+            correct += 1
+    return correct / float(len(images))
+
+# Load MNSIT
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("data/", one_hot=False)
+
+# Process data
+input_set = [[x] for x in mnist.train.images]
+
+with tf.Session() as sess:
+    # Run the 'init' op
+    sess.run(init_op)
+
+    for epoch in range(epochs):
+        print('===     Epoch ' + str(epoch) + '     ===')
+        for i, x in enumerate(input_set):
+            sess.run([layer.y, layer.learn], feed_dict={layer.x: x})
+            progress_bar(i / float(len(input_set)))
         print()
-        print('Clustering...')
-        counts = [0 for _ in range(classes)]
-        clusters = [np.mat(np.zeros((1, output_size))) for _ in range(classes)]
-        for input, c in zip(images, labels):
-            clusters[c] += layer.forward(input)
-            counts[c] += 1
-
-        for c in range(len(clusters)):
-            clusters[c] /= counts[c]
-
-        print(layer.permanence)
-        print('Validating')
-        correct = 0
-        for input, c in zip(images, labels):
-            # Find best match in cluster
-            best_class = None
-            min_norm = inf
-            output = layer.forward(input)
-            for k, cluster in enumerate(clusters):
-                diff = np.linalg.norm(cluster - output)
-                if diff < min_norm:
-                    min_norm = diff
-                    best_class = k
-
-            # Validate if best class is correct
-            if c == best_class:
-                correct += 1
-
-        print('Accuracy:', correct / float(len(images)))
-
-if __name__ == '__main__':
-    main()
+        #print(' == Clustering ==')
